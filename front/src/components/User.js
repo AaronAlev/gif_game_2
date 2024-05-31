f323a6a2f945089ce6c197028e8e3b5b06c95f99
xdgamerxdxd <joonas.palmik@voco.ee>
2024-05-31 13:33:53 +0300

    Leaderboard json only has numbers

--- front/src/components/User.js
+++ front/src/components/User.js
@@ -4,7 +4,7 @@ const User = ({ data }) => {
     return (
         <div className="Leaderboard-user">
             <h2> {data.Username} </h2>
-            <p> {data.Points} </p>
+            <p> {data.Points} points </p>
         </div>
     )
 }
