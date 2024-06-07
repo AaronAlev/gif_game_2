36b4dddfcbdef93e97477b47b08b89dfe5f4d57c
Lugeja <aaronalev@gmail.com>
2024-06-07 22:46:52 +0300

    Added dotenv and prep for rebase

--- front/src/App.js
+++ front/src/App.js
@@ -7,14 +7,14 @@ import UsernameScreen from './components/login.js';
 import axios from 'axios';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABEJ2CVtJwHV1XKV4kVmj4YhXcsIut5MA",
-  authDomain: "gif-game-71d17.firebaseapp.com",
-  databaseURL: "https://gif-game-71d17-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "gif-game-71d17",
-  storageBucket: "gif-game-71d17.appspot.com",
-  messagingSenderId: "899899259239",
-  appId: "1:899899259239:web:48a900f8080fa33817d7c2",
-  measurementId: "G-J6YV3WB8R2"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL
 };
 
 const app = initializeApp(firebaseConfig);
