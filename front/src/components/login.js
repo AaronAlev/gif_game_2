c5dd68d0fd5af555c8bd7e5318724616d431c19e
xdgamerxdxd <joonas.palmik@voco.ee>
2024-06-10 13:37:05 +0300

    Gif box start

--- front/src/components/login.js
+++ front/src/components/login.js
@@ -17,7 +17,7 @@ const handleUsername = (event, username, lobby_id, uid, setIsLoggedIn) => {
 const UsernameScreen = ({/*username, setUsername, lobby_id, setLobbyId, db, uid, setIsLoggedIn*/}) => {
     const {username, setUsername, lobby_id, setLobbyId, userUID, setIsLoggedIn} = useContext(GameContext);
     return (
-        <div>
+        <div className="login-container">
             <form onSubmit={(e) => handleUsername(e, username, lobby_id, userUID, setIsLoggedIn )} id="set-name">
                 <label>Username:</label>
                 <input type="text" autoComplete='off' value={username} onChange={(e) => setUsername(e.target.value)}/>
---
+++ front/src/gif.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Gif = () => {
+    return (
+        <div>
+            <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading"/>
+        </div>
+    )
+}
+
+export default Gif;
