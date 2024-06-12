c92190be393ab59fce1d3fb7bcb444a8649d82e1
Lugeja <aaronalev@gmail.com>
2024-06-12 11:57:56 +0300

    Sends delete lobby request to server on disconnect

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,13 @@
 // src/App.js
 import React, { useEffect } from 'react';
-import { signInAnonymously } from 'firebase/auth';
-import { ref, onValue } from 'firebase/database';
+import { signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import { ref, onValue, onDisconnect } from 'firebase/database';
 import GameContext from './gameContext.js';
 import UsernameScreen from './components/login.js';
 import Lobby from "./components/lobby.js";
 import { auth, db } from './firebase.js';
-import lobby from './components/lobby.js';
+import delete_lobby from './functions/reqDelLobby.js';
+
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
@@ -16,7 +17,17 @@ const App = () => {
   const [allMessagesRef, setAllMessagesRef] = React.useState([]);
   const [allPlayersRef, setAllPlayersRef] = React.useState([]);
 
-  useEffect(() => {
+  useEffect(() => { // Removes the player from database when they log out
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const playerReference = ref(db, `lobbies/${lobby_id}/players/${userUID}`);
+        onDisconnect(playerReference).remove();
+      }
+    });
+    return () => unsubscribe();
+  }, [lobby_id, userUID]);
+  
+  useEffect(() => { // Signs user in anonymously
     signInAnonymously(auth)
     .then(userCredential => {
       const user = userCredential.user;
@@ -30,7 +41,7 @@ const App = () => {
 
   useEffect(() => { // Reads all messages from the chat
     const chatRef = ref(db, `lobbies/${lobby_id}/chat`);
-    const unsubscribe = onValue(chatRef, (snapshot) => {
+    const readChat = onValue(chatRef, (snapshot) => {
       const chatData = snapshot.val();
       if (chatData) {
         const messageArray = Object.entries(chatData).map(([id, data]) => {
@@ -43,13 +54,13 @@ const App = () => {
       }
     });
 
-    return () => unsubscribe();
+    return () => readChat();
   }, [lobby_id]);
 
   useEffect(() => { // Reads all players from the database
     const allPlayersRef = ref(db, `lobbies/${lobby_id}/players`);
 
-    const unsubscribe = onValue(allPlayersRef, (snapshot) => {
+    const readPlayers = onValue(allPlayersRef, (snapshot) => {
       const playersData = snapshot.val();
       if (playersData) {
         const playersArray = Object.entries(playersData).map(([id, data]) => {
@@ -58,10 +69,13 @@ const App = () => {
           };
         });
         setAllPlayersRef(playersArray);
-      }
-    })
-    return () => unsubscribe();
-  }, [db, lobby_id]);
+      }})
+    return () => readPlayers();
+  }, [lobby_id, allPlayersRef]);
+
+  useEffect(() => {
+    delete_lobby(lobby_id)
+  }, [allPlayersRef])
 
   return (
     <GameContext.Provider value={ {username, setUsername, lobby_id, setLobbyId, userUID, setIsLoggedIn, allMessagesRef, allPlayersRef} }>
